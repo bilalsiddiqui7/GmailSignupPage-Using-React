241d6568352b17432a370bda87a2ea6934ef4426
Bilal <bilalsid0075@gmail.com>
2021-09-02 00:35:34 +0530

    SigninPage

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import MultipleInputs from './multipleInputs'
+import SignIn from './signin';
 
 function App() {
   return (
     <div>
-    <MultipleInputs /> 
+    <SignIn />
     </div>
   );
 }
